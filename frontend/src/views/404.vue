8a99be5c5adf658a29d62d7ece19c572f24998a5
zainab7681051 <zainab7681051@gmail.com>
2022-09-25 15:29:27 +0300

    m

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -6,6 +6,10 @@
               <h2 class="home__subtitle">Page Not Found</h2>
               <a href="/" class="button">Go Back</a>
           </div>
+
+          <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcaterville.files.wordpress.com%2F2013%2F10%2Fc8603-pizzadjcat.gif&f=1&nofb=1&ipt=964ec8b6912cd904925fe0854023b992c78eb8e3410f6473506ff452dda710b3&ipo=images" 
+          alt="404, page not found" 
+          class="home__img">
       </div>
   </section>
 </template>
